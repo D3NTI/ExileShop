f5a333200032cfa5ccfc959db23a45353cca8626
D3NTI <izmerdanik2007@gmail.com>
2025-11-10 13:16:39 +0100

    css rework commit

--- app/components/Pictures.tsx
+++ app/components/Pictures.tsx
@@ -14,28 +14,28 @@ export default function Pictures({ selectedCollection }: PicturesProps) {
   const collectionImages: { [key: string]: string } = {
     black:
       'https://basket-19.wbbasket.ru/vol3183/part318315/318315900/images/big/1.webp',
-    hb: 'https://basket-27.wbbasket.ru/vol5138/part513872/513872235/images/big/1.webp',
+    hb: 'https://basket-22.wbbasket.ru/vol3909/part390990/390990015/images/big/1.webp',
     tetris:
       'https://basket-27.wbbasket.ru/vol5138/part513872/513872235/images/big/1.webp',
     travel:
-      'https://images.unsplash.com/photo-1488646953014-85cb44e25828?w=800&h=600&fit=crop',
-    sup: 'https://basket-27.wbbasket.ru/vol5138/part513872/513872235/images/big/1.webp',
+      'https://basket-15.wbbasket.ru/vol2386/part238679/238679208/images/big/1.webp',
+    sup: 'https://basket-18.wbbasket.ru/vol2994/part299462/299462002/images/big/1.webp',
     mirror:
-      'https://basket-27.wbbasket.ru/vol5138/part513872/513872235/images/big/1.webp',
-    def: 'https://basket-27.wbbasket.ru/vol5138/part513872/513872235/images/big/1.webp',
+      'https://basket-20.wbbasket.ru/vol3366/part336639/336639719/images/big/1.webp',
+    def: 'https://basket-15.wbbasket.ru/vol2218/part221883/221883545/images/big/1.webp',
   };
   return (
     <div className="pictures-container">
       <div className="picture-wrapper">
+        <div className="picture-overlay">
+          <h2 className="picture-title">{selectedCollection.name}</h2>
+        </div>
         <img
           key={selectedCollection.id}
           src={collectionImages[selectedCollection.id]}
           alt={selectedCollection.name}
           className="picture-image"
         />
-        <div className="picture-overlay">
-          <h2 className="picture-title">{selectedCollection.name}</h2>
-        </div>
       </div>
     </div>
   );
